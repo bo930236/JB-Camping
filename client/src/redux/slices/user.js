92ada18702de8f0dd15280a0d0ad15b422e099f1
Bobo <bo930236@gmail.com>
2023-03-07 17:36:57 +0800

    ProfileScreen & Cart Icon

--- client/src/redux/slices/user.js
+++ client/src/redux/slices/user.js
@@ -1,9 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { resetUpdateSuccess, updateProfile } from '../actions/userActions';
 
 export const initialState = {
   loading: false,
   error: null,
-  userInfo: JSON.parse(localStorage.getItem('userInfo')) ?? null
+  userInfo: JSON.parse(localStorage.getItem('userInfo')) ?? null,
+  updateSuccess: false
 };
 
 export const userSlice = createSlice({
@@ -26,11 +28,20 @@ export const userSlice = createSlice({
     setError: (state, { payload }) => {
       state.error = payload;
       state.loading = false;
+    },
+    updateUserProfile: (state, { payload }) => {
+      state.userInfo = payload;
+      state.updateSuccess = true;
+      state.error = null;
+      state.loading = false;
+    },
+    resetUpdate: (state) => {
+      state.updateSuccess = false;
     }
   }
 });
 
-export const { setLoading, setError, userLogin, userLogout } = userSlice.actions;
+export const { setLoading, setError, userLogin, userLogout, updateUserProfile, resetUpdate } = userSlice.actions;
 export default userSlice.reducer;
 
 export const userSelector = (state) => state.user;
